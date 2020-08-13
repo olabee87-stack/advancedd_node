b15bcdbe3ba84b5a10dd8332d12c630c2bee6861
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-13 13:45:34 +0300

    Print last name and type of phones only

--- firstPhonesTest.js
+++ firstPhonesTest.js
@@ -1,10 +1,23 @@
 "use strict";
 const persons = require("./phones.json");
 
+//Print fname and last names of persons in the json file
 for (let person of persons) {
   console.log(`${person.firstname} ${person.lastname}`);
 
+  //Print phones of persons from the json file, phones
   for (let phone of person.phones) {
     console.log(`\t${phone.type} ${phone.number}`);
   }
+}
+
+for (let person of persons) {
+  console.log(`${person.lastname}`);
+  const types = [];
+  for (let phone of person.phones) {
+    if (!types.includes(phone.type)) {
+      types.push(phone.type);
+    }
+  }
+  console.log(`\t${types.join("\n\t")}`);
 }
