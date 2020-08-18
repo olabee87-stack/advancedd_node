700191e77d657c7b010c12ca0ffb6aa7ad7ac8c3
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-18 12:25:04 +0300

    second solution

--- telephones.js
+++ telephones.js
@@ -9,8 +9,8 @@ const phoneRegister = require("./phones.json");
 // };
 
 // console.log(getTypes());
-
-exports.getTypes = (type) => {
+//Solution one
+exports.getTypes = () => {
   let types = [];
   for (let person of phoneRegister) {
     for (let phone of person.phones) {
@@ -20,4 +20,23 @@ exports.getTypes = (type) => {
     }
   }
   return types;
+};
+
+//Solution 2 - Get numbers by type
+exports.getNumbersByType = (firstname, lastname, type) => {
+  if (firstname && lastname && type) {
+    const numbers = [];
+    for (let person of phoneRegister) {
+      if (person.firstname === firstname && person.lastname === lastname) {
+        for (let phone of person.phones) {
+          if (phone.type === type) {
+            numbers.push(phone.number);
+          }
+        }
+      }
+    }
+    return numbers;
+  } else {
+    throw new Error("Missing Parameter");
+  }
 };
