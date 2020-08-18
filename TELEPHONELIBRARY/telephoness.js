051cb92e4c92ce2260f19b9b586946a33b78ed3f
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-18 13:07:23 +0300

    Add working files

---
+++ TELEPHONELIBRARY/telephoness.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const phoneRegister = require("./phoness.json");
+
+//Solution one - Get types of numbers
+exports.getTypes = () => {
+  let types = [];
+  for (let person of phoneRegister) {
+    for (let phone of person.phones) {
+      if (!types.includes(phone.type)) {
+        types.push(phone.type);
+      }
+    }
+  }
+  return types;
+};
+
+//Solution 2 - Get numbers by type
+exports.getNumbersByType = (firstname, lastname, type) => {
+  if (firstname && lastname && type) {
+    const numbers = [];
+    for (let person of phoneRegister) {
+      if (person.firstname === firstname && person.lastname === lastname) {
+        for (let phone of person.phones) {
+          if (phone.type === type) {
+            numbers.push(phone.number);
+          }
+        }
+      }
+    }
+    return numbers;
+  } else {
+    throw new Error("Missing Parameter");
+  }
+};
+
+//Solution 3
+exports.getAllNumbersByType = (type) => {
+  if (!type) throw new Error("Missing parameter");
+  const numbersFound = [];
+  for (let person of phoneRegister) {
+    for (let phone of person.phones) {
+      if (phone.type === type) {
+        numbersFound.push({
+          firstname: person.firstname,
+          lastname: person.lastname,
+          number: { type: phone.type, tel: phone.number },
+        });
+      }
+    }
+  }
+  return numbersFound;
+};
+
+//Solution 4
+exports.getAllNumbers = () => phoneRegister;
---
+++ TELEPHONELIBRARY/telephonessTest.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const {
+  getTypes,
+  getNumbersByType,
+  getAllNumbersByType,
+  getAllNumbers,
+} = require("./telephoness");
+
+//Solution one log
+console.log(getTypes());
+
+//Solution two log
+console.log(getNumbersByType("Matt", "River", "home"));
+
+//Solution three log
+console.log(getAllNumbersByType("home"));
+
+//Solution 4
+console.log(JSON.stringify(getAllNumbers()));
