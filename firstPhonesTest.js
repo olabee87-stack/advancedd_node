514dd66b80093490ec170195db2cf7c374ba614d
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-18 11:14:47 +0300

    Modify

--- firstPhonesTest.js
+++ firstPhonesTest.js
@@ -21,3 +21,15 @@ for (let person of persons) {
   }
   console.log(`\t${types.join("\n\t")}`);
 }
+
+//Task 3
+
+for (let person of persons) {
+  //check type of number
+  for (let phone of person.phones) {
+    //print persons with 'home' phone only
+    if (phone.type === "home") {
+      console.log(`${person.firstname} ${person.lastname} ${phone.number}`);
+    }
+  }
+}
