9fedd791defa24a79bc628f95027c9c5ddf7c638
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-09-03 12:12:22 +0300

    Modify, working project

--- carServer/index.js
+++ carServer/index.js
@@ -1,11 +1,17 @@
 "use strict";
+
 const http = require("http");
+
+//Require all libraries
 const handleGetRequests = require("./library/routeHandlerGet.js")(__dirname);
 const handlePostRequests = require("./library/routeHandlerPost.js")(__dirname);
 const { redirectError } = require("./library/handler.js");
+
+//@Port definition
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+//@Server creation
 const server = http.createServer(async (req, res) => {
   if (req.method.toUpperCase() === "GET") {
     handleGetRequests(req, res);
@@ -15,6 +21,8 @@ const server = http.createServer(async (req, res) => {
     redirectError(res, "Resource not in use");
   }
 });
+
+//@Listening port
 server.listen(port, host, () =>
   console.log(`Server ${host} listens port ${port}`)
 );
--- carServer/library/handler.js
+++ carServer/library/handler.js
@@ -4,8 +4,10 @@
 const fs = require("fs");
 const path = require("path");
 
+//@Import mimetypes
 const MIMETYPES = require("./mimetypes.json");
 
+//@Read Function
 const read = (filepath) => {
   let extension = path.extname(filepath).toLowerCase();
   let mime = MIMETYPES[extension] || {
@@ -22,6 +24,8 @@ const read = (filepath) => {
     });
   });
 };
+
+//@Send function
 const send = (res, resource) => {
   res.writeHead(200, {
     "Content-Type": resource.mime.type,
@@ -32,24 +36,31 @@ const send = (res, resource) => {
   });
   res.end(resource.fileData, resource.mime.encoding);
 };
+
+//@Send json function
 const sendJson = (res, jsonResource) => {
   let jsonData = JSON.stringify(jsonResource);
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(jsonData);
 };
 
+//@Send status function
 const sendStatus = (res, message, statusCode = 200) => {
   res.writeHead(statusCode, {
     "Content-Type": "application/json; charset=utf-8",
   });
   res.end(JSON.stringify({ message }));
 };
+
+//@Send isIn function
 const isIn = (route, ...routes) => {
   for (let start of routes) {
     if (route.startsWith(start)) return true;
   }
   return false;
 };
+
+//@Get post function
 const getPostData = (req, contentType) =>
   new Promise((resolve, reject) => {
     if (req.headers["content-type"] !== contentType) {
@@ -60,17 +71,24 @@ const getPostData = (req, contentType) =>
         parse = require("querystring").parse;
       } else if (contentType === "application/json") {
         parse = JSON.parse;
+      } else {
+        return reject("Wrong Content-Type!");
       }
+      //Receives the data coming and delivers in chunks
       let databuffer = [];
       req.on("data", (messageFragment) => databuffer.push(messageFragment));
       req.on("end", () => resolve(parse(Buffer.concat(databuffer).toString())));
       req.on("error", () => reject("Error during the data transfer"));
     }
   });
+
+//@Redirect function
 const redirectError = (res, message) => {
   res.writeHead(303, { Location: `/error?message=${message}` });
   res.end();
 };
+
+//Export all functions
 module.exports = {
   read,
   send,
